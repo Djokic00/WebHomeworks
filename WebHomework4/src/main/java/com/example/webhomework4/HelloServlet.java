72304dbf4568e5bf889d512a4cc452d7b115e83d
Aleksa <adjokic1619rn@raf.rs>
2022-04-07 12:22:40 +0200

    Add get and post

--- WebHomework4/src/main/java/com/example/webhomework4/HelloServlet.java
+++ WebHomework4/src/main/java/com/example/webhomework4/HelloServlet.java
@@ -1,27 +1,105 @@
 package com.example.webhomework4;
 
 import java.io.*;
+import java.util.*;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
+
+    private List<String> days = new ArrayList<>();
+    private ArrayList<Meal> saveMeal = new ArrayList<>();
+    private Map<String, List<Meal>> map = new HashMap<>();
 
     public void init() {
-        message = "Hello World!";
+        days.add("Monday");
+        days.add("Tuesday");
+        days.add("Wednesday");
+        days.add("Thursday");
+        days.add("Friday");
+        getServletContext().setAttribute("days", days);
+        for(String day: days) {
+            try {
+                Scanner scanner = new Scanner(new File("/home/aleksa/Documents/JavaProjects/WebHomework/WebHomework4/src/database/" + day + ".txt"));
+                while (scanner.hasNextLine()) {
+                    String data = scanner.nextLine();
+                    Meal meal = new Meal(day, data);
+                    saveMeal.add(meal);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }}
     }
 
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("service method");
+        super.service(req, resp);
+    }
+
+    @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-
-        // Hello
         PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
+
+        if (getServletContext().getAttribute(request.getSession().getId()) != null) {
+            out.println("<h3>" + "Done" + "</h3>");
+            List<Meal> meals = map.get(request.getSession().getId());
+            for (Meal meal: meals) {
+                out.println(meal.getDay());
+                out.println("<br>");
+                out.println(meal.getFood());
+                out.println("<br>");
+                out.println("<br>");
+            }
+            return;
+        }
+
+        out.println("<html><body><form method=\"POST\" action = \"hello-servlet\">");
+        out.println("<h1>" + "Choose your lunch" + "</h1>");
+        for (String day: days) {
+            out.println("<h3>" + day + "</h3>");
+            out.println("<select name = \""+ day + "\" id=\"" + day + "\">");
+            System.out.println("<select name = \" "+ day + "\" id=\"" + day+ "\">" );
+            for (Meal meal: saveMeal) {
+                if (meal.getDay().equals(day)) {
+                    out.println("<option value = \"" + meal.getFood() + "\" selected>" + meal.getFood() + "</option>");
+                }
+            }
+            out.println("</select><br>");
+            out.println("--------------------------------");
+        }
+        out.println("<br><input type=\"submit\" name\"submit\" value\"Save\"/></form>");
         out.println("</body></html>");
+        getServletContext().setAttribute("allMeals", saveMeal);
     }
 
-    public void destroy() {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.sendRedirect("/response.html");
+        getServletContext().setAttribute(request.getSession().getId(),true);
+        List<Meal> list = new ArrayList<>();
+        for (String day: days) {
+            String meal = request.getParameter(day);
+            System.out.println(meal);
+            Meal o = returnMeal(day, meal);
+            o.setOrderNumber(o.getOrderNumber()  + 1);
+            list.add(o);
+        }
+        map.put(request.getSession().getId(), list);
+        getServletContext().setAttribute("map", map);
     }
+
+    private Meal returnMeal(String day, String meal){
+        for (Meal m : saveMeal){
+            if (m.getDay().equals(day) && m.getFood().equals(meal))
+                return m;
+        }
+        System.out.println(day + " " + meal);
+        return null;
+    }
+
+    public void destroy() {}
 }
---
+++ WebHomework4/src/main/java/com/example/webhomework4/Orders.java
@@ -0,0 +1,81 @@
+package com.example.webhomework4;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+@WebServlet(name = "Orders", value = "/orders")
+public class Orders extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        String password = request.getParameter("password");
+        Scanner scanner = new Scanner(new File("/home/aleksa/Documents/JavaProjects/WebHomework/WebHomework4/src/database/password.txt"));
+        if (password.equals(scanner.nextLine())) {
+            out.println("<html><body><form method=\"POST\" action = \"orders?password=aleksa\">");
+            out.println("<h1>" + "Meals" + "</h1>");
+            out.println("<html><body>");
+
+            List<Meal> listOfMeals = (List<Meal>) getServletContext().getAttribute("allMeals");
+            List<String> days = (List<String>) getServletContext().getAttribute("days");
+
+            for(String day: days) {
+                out.println("<h1>" + day + "</h1>");
+                out.println("<h1></h1>");
+                out.println("<table style=\"width:100 % \">");
+                out.println("<tr> <th>#</th> <th>Meal</th> <th>Quantity</th> </tr>");
+                int j = 0;
+                for (Meal listOfMeal : listOfMeals) {
+                    if (listOfMeal.getDay().equals(day)) {
+                        j++;
+                        out.println("<tr> <th>" + j + "</th> <th>" + listOfMeal.getFood() + "</th> <th>" + listOfMeal.getOrderNumber() + "</th> </tr>");
+                    }
+                }
+                out.println("<style>\n" +
+                        "table, th, td {\n" +
+                        "  border: 1px solid black;\n" +
+                        "}\n" +
+                        "</style></table>\n");
+                out.println("<h1></h1>");
+            }
+
+            out.println("--------------------------<br>");
+            out.println("<br><input type=\"submit\" name\"submit\" value\"Clear;\"/></form>");
+            out.println("</body></html>");
+        }
+        else {
+            out.println("<h3>" +"Wrong password" + "</h3>");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Meal> listOfMeals = (List<Meal>) getServletContext().getAttribute("allMeals");
+        for (Meal o: listOfMeals) {
+            o.setOrderNumber(0);
+        }
+        Map<String, List<Meal>> map = (Map<String, List<Meal>>) getServletContext().getAttribute("map");
+        for (Map.Entry<String, List<Meal>> m: map.entrySet()){
+            getServletContext().setAttribute(m.getKey(), null);
+        }
+        map = new HashMap<>();
+        getServletContext().setAttribute("map", map);
+
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+}
